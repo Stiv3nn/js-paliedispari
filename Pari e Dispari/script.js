05785b3f5444f02ee0eea17c7df9eb4b96811958
Stiven hoxha <stivenhoxha007@gmail.com>
2025-01-08 16:13:08 +0100

    pari o dispari esercizio

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -4,3 +4,30 @@
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+let sceltaUseers = prompt("scegli tra pari o dispari");
+const numeroUser  = parseInt(prompt("scegli un numero tra 1 e 5"));
+
+//console.log("Hai scelto:",sceltaUseers, numeroUser);  //DEBUG
+
+function pcNumRandom1To5() {
+    const numeroGenerato = Math.floor(Math.random() * 5) + 1;
+    return numeroGenerato;
+}
+
+let sommaTotale = numeroUser + pcNumRandom1To5();
+console.log("Somma dei due numeri:", sommaTotale);
+
+function pariODispari(sommaTotale) {
+
+    if(sommaTotale % 2 === 0) {
+        sommaTotale = "pari";
+    }   else {
+        sommaTotale = "dispari";
+    }   
+
+    return sommaTotale;
+    
+}
+
+console.log("La somma è:", pariODispari(sommaTotale));
